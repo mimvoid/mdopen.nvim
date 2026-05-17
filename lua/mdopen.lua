f5766edbdd7d011b7bdf26df69e6b860b4aa4c4b
mimvoid <mimvoid@proton.me>
2026-05-17 14:26:27 -0400

    Reorganize processes and functions

--- lua/mdopen.lua
+++ lua/mdopen.lua
@@ -5,8 +5,14 @@ local M = {}
 ---@field args? string[] Arguments to pass to mdopen commands
 local cfg = {}
 
----@type vim.SystemObj?
-local process = nil
+---@class mdopen_nvim.State
+---@field initalized boolean
+---@field mdopen_path? string Executable path of mdopen
+---@field process vim.SystemObj?
+local state = {
+  initialized = false,
+  process = nil,
+}
 
 --[[ Helper functions ]]
 
@@ -17,10 +23,6 @@ end
 ---Try to asynchronously install mdopen with cargo.
 ---@param on_success? fun() Callback to execute on successful installation
 local function install(on_success)
-  if vim.fn.executable("cargo") == 0 then
-    notify_err("Could not find cargo to install mdopen.")
-  end
-
   vim.system("cargo install mdopen", {}, function(opts)
     if opts.code ~= 0 then
       notify_err("Failed to install mdopen.")
@@ -30,72 +32,97 @@ local function install(on_success)
   end)
 end
 
+local function init_mdopen_path()
+  if cfg.mdopen_path then
+    -- Use the path in the user's config.
+    state.mdopen_path = cfg.mdopen_path
+    return
+  end
+
+  local exepath = vim.fn.exepath("mdopen")
+  if exepath ~= "" then
+    -- Found mdopen in the path.
+    state.mdopen_path = exepath
+  else
+    -- Try to install mdopen and get its path again.
+    install(function()
+      local new_path = vim.fn.exepath("mdopen")
+      if new_path ~= "" then
+        state.mdopen_path = new_path
+      end
+    end)
+  end
+end
+
 ---Execute mdopen for the currently opened buffer.
-local function run_mdopen()
-  if process ~= nil then
+---@param mdopen_path string
+---@param file string
+local function run_mdopen(mdopen_path, file)
+  if state.process then
+    state.process:kill("sigint")
+    state.process = nil
+  end
+
+  if vim.fn.executable(mdopen_path) == 0 then
+    notify_err(("Path for mdopen at %s does not exist."):format(mdopen_path))
     return
   end
 
-  if cfg.mdopen_path == nil then
-    notify_err("Could not find mdopen.")
-    return
+  local cmd = { mdopen_path }
+
+  if cfg.args and #cfg.args ~= 0 then
+    -- Extend cmd with args.
+    for i = 1, #cfg.args do
+      cmd[i + 1] = cfg.args[i]
+    end
   end
 
-  if vim.fn.executable(cfg.mdopen_path) == 0 then
-    notify_err(("Could not execute mdopen at %s."):format(cfg.mdopen_path))
-    return
-  end
+  local abspath = vim.fs.abspath(vim.fs.normalize(file))
 
-  local cmd = { cfg.mdopen_path }
-
-  if cfg.args ~= nil and #cfg.args ~= 0 then
-    table.insert(cmd, table.concat(cfg.args, " "))
-  end
-
-  -- mdopen doesn't work with absolute paths, so we split split it into the
-  -- directory and filename
-  local filepath = vim.api.nvim_buf_get_name(0)
-  table.insert(cmd, vim.fs.basename(filepath))
-
+  -- mdopen doesn't work with absolute paths, so we split it into the directory and filename
+  table.insert(cmd, vim.fs.basename(abspath))
   local job_opts = {
-    cwd = vim.fs.dirname(filepath),
-    stdout = function(_err, data)
-      vim.notify(data, vim.log.levels.INFO, { title = "mdopen.nvim" })
-    end,
-    stderr = function(_err, data)
-      notify_err(data)
+    cwd = vim.fs.dirname(abspath),
+    stdout = function(err, data)
+      if err then
+        print("mdopen.nvim Error:", err)
+      elseif data then
+        print("mdopen.nvim:", data)
+      end
     end,
     text = true,
   }
+  job_opts.stderr = job_opts.stdout -- Use the same callback.
 
-  process = vim.system(cmd, job_opts, function(_out)
-    process = nil
+  state.process = vim.system(cmd, job_opts, function(_out)
+    state.process = nil
   end)
 end
 
 --[[ Plugin module ]]
 
-function M.execute()
-  if cfg.mdopen_path ~= nil and cfg.mdopen_path ~= "" then
-    cfg.mdopen_path = vim.fn.exepath("mdopen")
+---@param file? string Path to the file to run mdopen on
+function M.run(file)
+  -- Default to full file name of current buffer
+  file = file or vim.api.nvim_buf_get_name(0)
+
+  if not state.initalized then
+    init_mdopen_path()
   end
 
-  if cfg.mdopen_path ~= "" then
-    run_mdopen()
+  if state.mdopen_path then
+    run_mdopen(state.mdopen_path, file)
   else
-    -- Couldn't find mdopen in the config or exepath, try installing
-    install(function()
-      M.mdopen_path = vim.fn.exepath("mdopen")
-      run_mdopen()
-    end)
+    notify_err("Could not find mdopen.")
   end
 end
 
 function M.stop()
-  if process == nil then
-    notify_err("Could not find the process for mdopen.")
+  if state.process then
+    state.process:kill("sigint")
+    state.process = nil
   else
-    process:kill("sigint")
+    notify_err(("No mdopen process at %s."):format(file))
   end
 end
 
@@ -105,13 +132,24 @@ function M.setup(config)
   cfg = config or cfg
 
   -- Add plugin commands
-  vim.api.nvim_create_user_command("Mdopen", function(_opts)
-    M.execute()
-  end, { complete = "file", nargs = "?", bang = true })
+  vim.api.nvim_create_user_command("Mdopen", function(args)
+    M.run(args.fargs[1])
+  end, { complete = "file", nargs = "?" })
 
-  vim.api.nvim_create_user_command("MdopenStop", function(_opts)
+  vim.api.nvim_create_user_command("MdopenStop", function(_args)
     M.stop()
-  end, { complete = "file", nargs = "?", bang = true })
+  end, {})
+
+  local augroup = vim.api.nvim_create_augroup("mdopen", {})
+  -- Kill all processes when exiting
+  vim.api.nvim_create_autocmd("VimLeavePre", {
+    group = augroup,
+    callback = function()
+      if state.process then
+        state.process:kill("sigint")
+      end
+    end,
+  })
 end
 
 return M
